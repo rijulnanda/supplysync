1764aa135a91d3db5a1f0edef2641414241f2d18
rijulnanda <31491803+rijulnanda@users.noreply.github.com>
2024-12-26 11:58:14 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import streamlit as st
+import json
 
-# Retrieve credentials and database URL from secrets
+# Retrieve Firebase credentials and database URL from secrets
 firebase_credentials = st.secrets["firebase"]["credentials"]
 database_url = st.secrets["firebase"]["database_url"]
 
+# Parse the JSON string into a dictionary
+cred_dict = json.loads(firebase_credentials)
+
 # Initialize Firebase with the credentials and database URL
-cred = credentials.Certificate(firebase_credentials)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred, {
     'databaseURL': database_url
 })
 
-# Fetch data from Firebase
+# Fetch data from Firebase to test connection
 ref = db.reference('test')  # Reference to a test node
 data = ref.get()
 
+# Display data in Streamlit
 st.write(data)
